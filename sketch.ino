9bc1068fdabea3fd21f6c1c6b40f44e196e500b8
Riley <97705302+Rileynotmyalt@users.noreply.github.com>
2022-02-28 09:05:34 -0600

    initial commit

---
+++ sketch.ino
@@ -0,0 +1,13 @@
+/**
+ * This is Riley Carter's Computer Science Principals Arduino Project
+ * This program displays a character on a lcd display and the player
+ * can navigate through a course using buttons
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
